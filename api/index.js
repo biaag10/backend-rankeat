d3985c2cd1f5782ad4b38dd7a8701a762f92cd3c
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-06-28 17:57:57 -0300

    mudanca na liberacao de cors

--- api/index.js
+++ api/index.js
@@ -17,12 +17,13 @@ db.connect();
 
 const app = express();
 
-
 // Configuração do CORS
 app.use(cors({
-  origin: '*', // permite requisições de qualquer origem
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // cabeçalhos permitidos
+  origin: '*',  // Permite qualquer origem, você pode adicionar URLs específicas no lugar do '*'
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],  // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeçalhos permitidos
+  preflightContinue: true, // Habilita a resposta para as requisições OPTIONS
+  optionsSuccessStatus: 204  // Algumas versões antigas do Node podem precisar dessa configuração para o status correto
 }));
 
 app.use(express.json()); // para aceitar JSON no corpo das requisições
