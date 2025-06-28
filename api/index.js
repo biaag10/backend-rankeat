2ee6c8b1967bb3b325399d19dfd4c132533af803
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-06-28 18:05:35 -0300

    mudanca na liberacao de cors

--- api/index.js
+++ api/index.js
@@ -17,14 +17,14 @@ db.connect();
 
 const app = express();
 
+// Configuração do CORS
+app.use(cors());
 
 // Configuração do CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://rank-eat.vercel.app'], // permite requisições do frontend
+  origin: ['http://localhost:5173', 'https://automatic-eureka-pvwwxgrpxr937p5r-5173.app.github.dev/'], // permite requisições do frontend
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // cabeçalhos permitidos
-  // preflightContinue: true, // Habilita a resposta para as requisições OPTIONS
-  // optionsSuccessStatus: 204  // Algumas versões antigas do Node podem precisar dessa configuração para o status correto
 }));
 
 app.use(express.json()); // para aceitar JSON no corpo das requisições
